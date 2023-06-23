5b1ee46122015a5f494e05169df2dfcf1657e9e0
Amanda <afholdenried@gmail.com>
2023-06-23 12:41:41 -0500

    8kyu multiply

---
+++ 8kyu-multiply.js
@@ -0,0 +1,12 @@
+//Description
+//This code does not execute properly. Try to figure out why.
+
+//Solution 1
+//------------------------------------------------------------
+function multiply(a, b) {
+  return a * b
+}
+
+//Solution 2
+//------------------------------------------------------------
+multiply = (a, b) => a * b
